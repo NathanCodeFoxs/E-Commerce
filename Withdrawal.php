2e94eb28bd9e25e6029af68e8f510edbe5c6e275
Eric Nathan A. Delacruz <nathancodefoxs@gmail.com>
2025-12-16 13:12:58 +0800

    Needed to be fix sidebar
    Jacent

--- Withdrawal.php
+++ Withdrawal.php
@@ -190,7 +190,7 @@
         <a onclick="goTo('Bills.php')"><img src="Images/Bill.png" width="20"> Bills</a>
         <a onclick="goTo('Loan.php')"><img src="Images/Loan.png" width="20"> Loan</a>
         <a onclick="goTo('Deposit.php')"><img src="Images/Safe_In.png" width="20"> Deposit</a>
-        <a onclick="goTo('Withdraw.php')"><img src="Images/Safe_Out.png" width="20"> Withdrawal</a>
+        <a onclick="goTo('Withdrawal.php')"><img src="Images/Safe_Out.png" width="20"> Withdrawal</a>
         <a onclick="goTo('Finance.php')"><img src="Images/Finance.png" width="20"> Finance</a>
         <a onclick="goTo('settings.php')"><img src="Images/Setting.png" alt="" width="20"> Settings</a>
         <a onclick="goTo('logout.php')"><img src="Images/Logout.png" alt="" width="20"> Logout</a>
